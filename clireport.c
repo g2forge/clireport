5f2c37ca919fe5dd78a215758c4743bb66c2d980
Greg Gibeling <gdgib@outlook.com>
2024-02-27 19:34:49 -0800

    G2-1540 Code CLI tester

---
+++ clireport.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("CLIReport: %d arguments\n", argc);
+	for (int i = 0; i < argc; i++) {
+		char* argument = argv[i];
+		printf("%04d: %s\n", (unsigned)strlen(argument), argument);
+	}
+	return 0;
+}
